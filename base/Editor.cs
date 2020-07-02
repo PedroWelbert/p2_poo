6506f03766076891dca2d05f188e8fe15651b01a
PedroWelbert <you@example.com>
2020-07-02 00:09:26 -0300

    Diagrama and classes

---
+++ base/Editor.cs
@@ -0,0 +1,17 @@
+public class Editor : __abstract__Usuario
+{
+	private List<Post> posts;
+
+	private string assinatura;
+
+	public List<Post> Posts{ get; set; }
+
+	public string Assinatura{ get; set; }
+
+	public void imprimirDados()
+	{
+
+	}
+
+}
+
---
+++ base/IntrumentoMusical.cs
@@ -0,0 +1,17 @@
+public class IntrumentoMusical
+{
+	private string nome;
+
+	private string tipo;
+
+	public string Nome{ get; set; }
+
+	public string Tipo{ get; set; }
+
+	public void imprimirDados()
+	{
+
+	}
+
+}
+
